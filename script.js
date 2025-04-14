6dbe6ce25215e69bd7df3834268042b6a5be9d28
ggg-b <giulia.barbero.sup@gmail.com>
2025-04-14 11:26:16 +0000

    .

---
+++ script.js
@@ -0,0 +1,6 @@
+// Gestisce il click sul bottone "Acquista"
+document.getElementById('acquista-btn').addEventListener('click', () => {
+    alert('Grazie per aver acquistato!'); // Mostra un messaggio di conferma
+    // Puoi aggiungere qui altre funzionalità, come il reindirizzamento
+    // window.location.href = 'pagina-acquisto.html'; // Esempio di reindirizzamento
+});
